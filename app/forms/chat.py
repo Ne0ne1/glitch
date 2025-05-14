15c3a28b3e986f1bdf4c1bbe75171147c5a974f9
Ne0ne <isadadashev777@gmail.com>
2025-05-14 15:51:15 +0300

    4

--- app/forms/chat.py
+++ app/forms/chat.py
@@ -1,15 +1,19 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, TextAreaField, SubmitField
 from wtforms.validators import DataRequired
+from flask_wtf.file import FileField, FileAllowed
 
 
-# Форма чата
+# Форма создания нового чата
 class NewChatForm(FlaskForm):
     username = StringField('Имя пользователя', validators=[DataRequired()])
     submit = SubmitField('Начать чат')
 
 
-# Форма сообщения
+# Форма отправки сообщения
 class MessageForm(FlaskForm):
     text = TextAreaField('Сообщение', validators=[DataRequired()])
+    image = FileField('Изображение', validators=[
+        FileAllowed(['jpg', 'jpeg', 'png', 'webp'], 'Только изображения!')
+    ])
     submit = SubmitField('Отправить')
